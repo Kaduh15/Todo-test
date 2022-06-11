49d6e190c5ec765167db18a90250c90f4df27e31
Kadu <kadu.silva2014@gmail.com>
2022-06-11 13:19:55 -0300

    mostra a descrição da task quando clicado no icone de info

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -7,7 +7,6 @@ const Container = styled.div`
   flex-direction: column;
 
   width: 993px;
-  height: 121px;
 
   padding: 25px 51px;
   margin-top: 30px;
@@ -31,31 +30,73 @@ const Container = styled.div`
     line-height: 58px;
   }
 
+  .btn {
+    background-color: Transparent;
+    background-repeat: no-repeat;
+    border: none;
+  }
+
   svg {
     width: 72px;
     height: 72px;
 
     color: ${(props) => (props.isComplete ? '#12CC3B' : '#AA2C2C')};
   }
+
+  .description {
+    width: 891px;
+    height: 287px;
+
+    padding: 19px 30px;
+    margin-top: 30px;
+
+    border-width: 5px 0px;
+    border-style: solid;
+    border-color: ${(props) => (props.isComplete ? '#12CC3B' : '#AA2C2C')};
+    border-radius: 30px;
+
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+  }
 `;
 
-export default class Taks extends Component {
+export default class Task extends Component {
   state = { showDescription: false };
+
+  ToggleShowDescription = () => {
+    const { showDescription } = this.state;
+
+    this.setState({ showDescription: !showDescription });
+  };
 
   render() {
     const { title, description, isComplete } = this.props;
     const { showDescription } = this.state;
     return (
-      <Container isComplete={isComplete}>
+      <Container onDoubleClick={() => console.log('click')} isComplete={isComplete}>
         <div className="div-title">
           <p className="title">{title}</p>
           <div className="icons">
-            <AiFillInfoCircle />
-            <AiFillCloseCircle />
+            <button
+              className="btn"
+              type="button"
+              onClick={this.ToggleShowDescription}
+            >
+              <AiFillInfoCircle />
+            </button>
+            <button className="btn" type="button">
+              <AiFillCloseCircle />
+            </button>
           </div>
         </div>
 
-        {showDescription && <p className="description">{description}</p>}
+        {showDescription && (
+          <div className="descriprion">
+            <p className="description">{description}</p>
+          </div>
+        )}
       </Container>
     );
   }
